aa74fa77361e3672f605f2d28180ec611356c077
Audey <audeygodfrey@gmail.com>
2022-09-22 17:07:56 +0100

    final commit

--- src/script.js
+++ src/script.js
@@ -1,17 +1,22 @@
 function setFormMessage(formElement, type, message) {
      const messageElement = formElement.querySelector(".form__message");
-
+ 
      messageElement.textContent = message;
      messageElement.classList.remove("form__message--success", "form__message--error");
-     messageElement.classList.add("form__message--${type}`");
-}
-
-function setInputError(inputElement, message) {
-     inputElement.classList.add("form__input--errror")
+     messageElement.classList.add(`form__message--${type}`);
+ }
+ 
+ function setInputError(inputElement, message) {
+     inputElement.classList.add("form__input--error");
      inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
-}
-
-document.addEventListener("DOMContentLoaded", () => {
+ }
+ 
+ function clearInputError(inputElement) {
+     inputElement.classList.remove("form__input--error");
+     inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+ }
+ 
+ document.addEventListener("DOMContentLoaded", () => {
      const loginForm = document.querySelector("#login");
      const createAccountForm = document.querySelector("#createAccount");
  
@@ -26,12 +31,24 @@ document.addEventListener("DOMContentLoaded", () => {
          loginForm.classList.remove("form--hidden");
          createAccountForm.classList.add("form--hidden");
      });
-
+ 
      loginForm.addEventListener("submit", e => {
-          e.preventDefault();
-
-          //peform your submit
-
-          setFormMessage(loginForm, "error", "Invalid username/password combanation.");
+         e.preventDefault();
+ 
+         // Perform your AJAX/Fetch login
+ 
+         setFormMessage(loginForm, "error", "Invalid username/password combination");
      });
-});
+ 
+     document.querySelectorAll(".form__input").forEach(inputElement => {
+         inputElement.addEventListener("blur", e => {
+             if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 4) {
+                 setInputError(inputElement, "Username must be at least 4 characters in length");
+             }
+         });
+ 
+         inputElement.addEventListener("input", e => {
+             clearInputError(inputElement);
+         });
+     });
+ });
